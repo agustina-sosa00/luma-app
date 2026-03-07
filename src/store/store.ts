022a0b517dfbba3f4f345c406dd30b02b9fe73d3
Agustina Sosa <agustinaayelensosa2@gmail.com>
2026-03-07 01:53:54 -0300

    mapeo de categorias

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
 import appReducer from './app/appSlice';
+import { appServices } from '@/services/appServices';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     app: appReducer,
+    [appServices.reducerPath]: appServices.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(appServices.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
